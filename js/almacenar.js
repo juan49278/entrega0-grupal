9e9462dba72529197efa31e1d0ee88b061f0a3d2
juan49278 <juan49278@gmail.com>
2022-08-28 22:07:12 -0300

    update

--- js/almacenar.js
+++ js/almacenar.js
@@ -3,23 +3,23 @@ function add(){
     if(text.value == ""){
         text.classList.add("is-invalid");
     } else {
-        document.getElementById("contenedor").innerHTML += `
+        contenedor.innerHTML += `
         <li class="list-group-item">${text.value}</li>`
         text.classList.remove("is-invalid");
     }
     text.value = "";
-    let cont = document.getElementById("contenedor");
-    localStorage.setItem("content", cont.innerHTML)
+   // let cont = document.getElementById("contenedor");
+    localStorage.setItem("content", contenedor.innerHTML)
 }
 
 agregar.addEventListener("click", add);
 
 document.addEventListener("DOMContentLoaded", () =>{
-    document.getElementById("contenedor").innerHTML = localStorage.getItem("content")
+    contenedor.innerHTML = localStorage.getItem("content")
 })
 
 limpiar.addEventListener("click", ()=>{
-    document.getElementById("contenedor").innerHTML = "";
+    contenedor.innerHTML = "";
     localStorage.removeItem("content");
     text.classList.remove("is-invalid");
 })
