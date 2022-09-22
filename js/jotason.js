425a6ed41d4f5ec86f1a2daa02ec033b41d5a739
francosat28 <110430417+francosat28@users.noreply.github.com>
2022-09-21 22:49:42 -0300

    commit

--- js/jotason.js
+++ js/jotason.js
@@ -6,14 +6,15 @@ document.addEventListener("DOMContentLoaded", async()=>{
     })
 btnBuscar.addEventListener("click", ()=>{
     let val = inputBuscar.value.toLowerCase();
-    let printArray = result.filter(movie=>movie.title.toLowerCase().includes(val));
+    let printArray = result.filter(movie=>(movie.title.toLowerCase().includes(val)))/*||(movie=>movie.overview.toLowerCase().includes(val))||
+    (movie=>movie.genres.name.toLowerCase().includes(val))||(movie=>movie.tagline.toLowerCase().includes(val)));*/
     for(let movies of printArray){
         lista.innerHTML+=`
         <ul class="list-group">
         <li class="list-group-item d-flex justify-content-between align-items-start"  style="background:none;color:#fff;">
           <div class="ms-2 me-auto">
             <div class="fw-bold">${movies.title}</div>
-                ${movies.overview}
+                ${movies.tagline}
             </div>
           <span class="badge bg-primary rounded-pill">estrellita</span>
         </li>
