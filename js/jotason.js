647b597f638523f095bec961255f85f9a0f72c43
GabrielGonzalez <gabriel-97-312@hotmail.com>
2022-09-27 23:26:03 -0300

    offCanvas

--- js/jotason.js
+++ js/jotason.js
@@ -11,15 +11,23 @@ btnBuscar.addEventListener("click", ()=>{
     let printArray = result.filter(movie=>(movie.title.toLowerCase().includes(val)));
     for(let movies of printArray){
         lista.innerHTML+=`
-        <ul class="list-group">
-        <li class="list-group-item d-flex justify-content-between align-items-start"  style="background:none;color:#fff;">
+        <a style="color: white; background-color: transparent;" data-bs-toggle="offcanvas" href="#offcanvasExample" class="list-group-item list-group-item-action d-flex justify-content-between align-items-start" aria-current="true">
+          <div class="ms-2 me-auto">
+          <div class="fw-bold">${movies.title}</div>
+            <small class="text-muted">${movies.tagline}</small>
+          </div>
+          <span class="badge bg-primary rounded-pill">estrellitas</span>
+        </a>
+        
+        
+        `
+    }
+})
+/*
+<li class="list-group-item cursor-active d-flex justify-content-between align-items-start" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"  style="background:none;color:#fff;">
           <div class="ms-2 me-auto">
             <div class="fw-bold">${movies.title}</div>
               <small class="text-muted">${movies.tagline}</small>
             </div>
           <span class="badge bg-primary rounded-pill">estrellitas</span>
-        </li>
-        </ul>
-        `
-    }
-})
+        </li>*/
